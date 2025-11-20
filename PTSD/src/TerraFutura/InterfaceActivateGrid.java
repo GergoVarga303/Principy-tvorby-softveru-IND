aebccf19da8ced0f04328d6c16471c7046beddbc
DavidMonis <david5.monis@gmail.com>
2025-11-20 17:48:58 +0100

    GreenClass

---
+++ PTSD/src/TerraFutura/InterfaceActivateGrid.java
@@ -0,0 +1,7 @@
+package TerraFutura;
+
+import java.util.Collection;
+import java.util.AbstractMap.SimpleEntry;
+public interface InterfaceActivateGrid {
+    void setActivationPattern(Collection<SimpleEntry<Integer, Integer>> pattern);
+}
