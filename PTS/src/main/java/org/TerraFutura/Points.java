38f1f6e6b200abb500e43dccca22984bdd24cc89
Kurnava <molnar.peto222@gmail.com>
2025-11-26 01:02:30 +0100

    Add constructor and getter for Points class

--- PTS/src/main/java/org/TerraFutura/Points.java
+++ PTS/src/main/java/org/TerraFutura/Points.java
@@ -1,4 +1,16 @@
 package org.TerraFutura;
 
 public class Points {
+    private final int value;
+
+    public Points(int value) {
+        if (value < 0) {
+           throw new IllegalArgumentException("The value is less then 0");
+        }
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
