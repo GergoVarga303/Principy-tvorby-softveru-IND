f4fd81d83d9faa39fff800138eb479cbe85c9e82
DavidMonis <david5.monis@gmail.com>
2025-12-01 21:00:18 +0100

    Import

--- PTS/src/main/java/org/TerraFutura/TransformationFixed.java
+++ PTS/src/main/java/org/TerraFutura/TransformationFixed.java
@@ -3,8 +3,7 @@ package org.TerraFutura;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * TransformationFixed represents a fixed transformation effect defined by:
